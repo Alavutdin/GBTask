4e8ffa2866db412de3ecbe1aa090bbd2f38d98ca
atlan <alavutdin_atlan@mail.ru>
2022-12-24 21:46:36 +0300

    Add Task23

---
+++ Homework/Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число от 1 до ");
+int.TryParse(ReadLine(), out int n);
+
+while(n>0)
+{
+    WriteLine($"Квадрат числа {n} = {Math.Pow(n, 3)}");
+    n--;
+}
